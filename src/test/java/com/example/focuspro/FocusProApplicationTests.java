73fa4c3c402cdb0a0a4bd428816d6ddd0f47bf7c
Ahmad Hammoud <hammoudahmad530@gmail.com>
2025-09-17 22:39:42 +0300

    Initial commit

---
+++ src/test/java/com/example/focuspro/FocusProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.focuspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
