bd9757aedbe9dbba4aa4651d12ccf9669ec80669
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2025-09-18 17:45:56 +0300

    l

--- src/main/java/com/example/focuspro/services/UserService.java
+++ src/main/java/com/example/focuspro/services/UserService.java
@@ -18,25 +18,29 @@ public class UserService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
-
     public void register(Users user) {
-        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             throw new IllegalArgumentException("Username not available");
         }
-        if(user.getPassword().isEmpty()){
+        if (user.getPassword().isEmpty()) {
             throw new IllegalArgumentException("Password cannot be empty");
         }
-        if(user.getEmail().isEmpty()){
+        if (user.getEmail().isEmpty()) {
             throw new IllegalArgumentException("Email cannot be empty");
         }
-        if(user.getName().isEmpty()){
+        if (user.getName().isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty");
         }
-        if(user.getDob() == null){
+        if (user.getDob() == null) {
             throw new IllegalArgumentException("Date of birth cannot be empty");
         }
-        if(user.getCreatedAt() == null){
+        if (user.getCreatedAt() == null) {
+            throw new IllegalArgumentException("Date of birth cannot be empty");
+        }
+        if (user.getDeletedAt() == null) {
+            throw new IllegalArgumentException("Date of birth cannot be empty");
+        }
+        if (user.getConsentAt() == null) {
             throw new IllegalArgumentException("Date of birth cannot be empty");
         }
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
