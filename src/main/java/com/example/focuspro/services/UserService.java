e031ea3b81d13584082fd445b064b1e3d4df28f4
Ahmad Hammoud <hammoudahmad530@gmail.com>
2025-09-18 17:16:04 +0300

    Fixed the Register method to check for nullness of necessary variables.

--- src/main/java/com/example/focuspro/services/UserService.java
+++ src/main/java/com/example/focuspro/services/UserService.java
@@ -18,6 +18,21 @@ public class UserService {
 
 
     public void register(Users user) {
+        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+            throw new IllegalArgumentException("Username not available");
+        }
+        if(user.getPassword().isEmpty()){
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if(user.getEmail().isEmpty()){
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+        if(user.getName().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if(user.getDob() == null){
+            throw new IllegalArgumentException("Date of birth cannot be empty");
+        }
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
