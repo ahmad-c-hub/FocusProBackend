a41868bb1c6330597748e15cb98940b31e3c2dc7
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2025-10-01 14:02:34 +0300

    here

--- src/main/java/com/example/focuspro/controllers/UserController.java
+++ src/main/java/com/example/focuspro/controllers/UserController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(
         origins = {
                 "http://localhost:3000",   // React
-                "http://10.0.2.2:8080"     // Android emulator access
+                "http://10.0.2.2:8080",
+                "http://localhost:5000/"     // Android emulator access
         },
         allowedHeaders = "*",
         methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}
